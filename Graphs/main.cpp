72b071769f05d5f9318b803824e91b7918583a8e
Miłosz Skobejko <miloszskobejko@icloud.com>
2023-07-17 15:34:23 +0200

    Initial Commit

---
+++ Graphs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Graphs
+//
+//  Created by Miłosz Skobejko on 17/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
