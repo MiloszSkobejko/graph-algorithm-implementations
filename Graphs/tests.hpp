13ec1b8fe32d153d7636ea170e2c79f439a63dd4
Miłosz Skobejko <miloszskobejko@icloud.com>
2023-08-13 23:50:24 +0200

    added island count, biggest island and shortest path algorithms. Created file for tests. Test are only prototypes at this moment.

---
+++ Graphs/tests.hpp
@@ -0,0 +1,23 @@
+//
+//  tests.hpp
+//  Graphs
+//
+//  Created by Miłosz Skobejko on 13/08/2023.
+//
+
+#ifndef tests_hpp
+#define tests_hpp
+
+#include <cassert>
+#include <stdio.h>
+#include "graph.hpp"
+
+void test_bfs();
+void test_dfs();
+void test_hasPath();
+void test_count_connected_components();
+void test_largest_component();
+void test_island_count();
+void test_shortest_path();
+
+#endif /* tests_hpp */
